d0e442a7a6d542e52c7af0f435f79c61d1bff09e
Cristiano Ap Lazaro <cristianoalazaro@hotmail.com>
2020-08-31 19:03:30 -0300

    alteracao no app.js

--- app.js
+++ app.js
@@ -21,11 +21,8 @@ mongoose
     console.log('Erro ao conectar com o mongoDB: ' + err);
   });
 
-<<<<<<< HEAD
 app.put('/student/:id', async(((req, res) = {})));
 
-=======
->>>>>>> 962dc0074817d49b81bf6161aaac1e635a742fd5
 const app = express();
 app.use(express.json());
 app.use(accountRouter);
